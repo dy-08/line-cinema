dafa421ed6a39b03c3cf0f3b3600c8ff619e6bfb
Youngho Kwon <127571276+dy-08@users.noreply.github.com>
2025-10-14 16:45:19 +0900

    feat: 공통 레이아웃 및 기본 UI/비동기 렌더링 기능 구현
    * wip: develop 브렌치 생성 및 초기 커밋
    * wip: feat/common-layout 브렌치 생성 및 초기 커밋
    * wip: 헤더 마크업 및 기본 스타일 작업 중 (로고 등 미정)
    * wip: 푸터 마크업 및 스타일 1차 완성 (회의 후 수정 가능성 있음)
    * wip: 네비게이션 hover 효과 1차 구현 (동작 방식 논의 필요)
    * test: TMDB API 키 import 후 데이터 응답 테스트 코드 추가
    * setup: 작업공간 분리
    * feat: 로고 및 이미지 추가, 관련 스타일 수정
    * feat: 페이지 비동기 로딩 및 렌더링 기능 구현

--- src/js/pages/main.js
+++ src/js/pages/main.js
@@ -4,39 +4,60 @@ import { API_KEYS } from '../config/config.js'; // 키 요청
 const navi = document.querySelectorAll('.header-navi-main');
 let current = '';
 navi.forEach((item) => {
-  item.addEventListener('mouseover', () => {
-    if (current) {
-      current.classList.remove('selected');
-      current = item.nextElementSibling;
-      current.classList.add('selected');
-    }
-    current = item.nextElementSibling;
-    current.classList.add('selected');
+    item.addEventListener('mouseover', () => {
+        if (current) {
+            current.classList.remove('selected');
+            current = item.nextElementSibling;
+            current.classList.add('selected');
+        }
+        current = item.nextElementSibling;
+        current.classList.add('selected');
 
-    current.addEventListener('mouseleave', () => {
-      current.classList.remove('selected');
+        current.addEventListener('mouseleave', () => {
+            current.classList.remove('selected');
+        });
     });
-  });
 });
 
 // TMDB API 테스트 코드
 // console.log(API_KEYS); // 키 응답
 async function tmdb() {
-  try {
-    const options = { method: 'GET', headers: { accept: 'application/json' } };
-    // v3
-    const res = await fetch(
-      `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEYS.TMDB}&language=ko-KR`,
-      options
-    );
-    const data = await res.json();
-    console.log(data);
-  } catch (e) {
-    console.error(e);
-  }
+    try {
+        const options = { method: 'GET', headers: { accept: 'application/json' } };
+        // v3
+        const res = await fetch(
+            `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEYS.TMDB}&language=ko-KR`,
+            options
+        );
+        const data = await res.json();
+        console.log(data);
+    } catch (e) {
+        console.error(e);
+    }
 }
 tmdb();
 
+async function renderPage(data) {
+    document.getElementById('app').innerHTML = await data;
+}
+async function fetchPage(page) {
+    try {
+        const res = await fetch(`./src/html/${page}.html`);
+        if (!res.ok) throw new Error('page is not found');
+        const data = await res.text();
+        return data;
+    } catch (error) {
+        console.error('fetchPageError:', error.message);
+    }
+}
+document.querySelectorAll('.header-navi-sub').forEach((item) =>
+    item.addEventListener('click', async (e) => {
+        const page = e.target.dataset.page.slice(6);
+        const result = await fetchPage(page);
+        if (!result) return;
+        await renderPage(result);
+    })
+);
 // 지유님: 슬라이더 작업섹션
 
 // 철원님: 랭킹 작업섹션
