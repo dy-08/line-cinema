c358ae074db882c2ed317078565ba57c5c80c09f
dlwldb <dlwldb0506@naver.com>
2025-10-15 17:26:06 +0900

    wip: 슬라이드 TMDB API연동 작업진행중

--- src/js/pages/main.js
+++ src/js/pages/main.js
@@ -21,21 +21,69 @@ navi.forEach((item) => {
 
 // TMDB API 테스트 코드
 // console.log(API_KEYS); // 키 응답
+const slidePath = [];
 async function tmdb() {
   try {
     const options = { method: 'GET', headers: { accept: 'application/json' } };
     // v3
     const res = await fetch(
-      `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEYS.TMDB}&language=ko-KR`,
+      `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEYS.TMDB}&language=ko-KR/`,
       options
     );
     const data = await res.json();
-    console.log(data.results[0].backdrop_path);
-  } catch (e) {
+    // console.log(data.results);
+    
+    // 파싱
+    data.results.forEach((item, idx) => {
+      // backdrop_path
+      if(idx<5) slidePath.push(item.backdrop_path);
+    });
+    } catch (e) {
     console.error(e);
   }
+    // const div1 = document.createElement('div')
+    // div1.className = `main-img img5`; 
+    // forEach(item, index) {
+    //   let path = data.results[item.length].backdrop_path
+    //   const div = document.createElement('div')
+    //   div.className = `main-img img${index+1}`;
+    //   div.style.background = url(`https://image.tmdb.org/t/p/w500/${path}`)
+      // https://image.tmdb.org/t/p/w500/9DYFYhmbXRGsMhfUgXM3VSP9uLX.jpg
+      // }
+      
+      // const div2 = document.createElement('div')
+      // div.className = `main-img img1`;   
 }
 tmdb();
+
+  function makeImg() {
+    const imgBox = document.querySelector(".main-inBox-imgbox");
+    console.log(slidePath);
+    
+    slidePath.forEach(item => {
+      console.log('item: ',item);
+      
+    });
+    // const div1 = document.createElement('div');
+    // div1.className = 'main-img img1';
+    // const div2 = document.createElement('div');
+    // div2.className = 'main-img img2';
+    // const div3 = document.createElement('div');
+    // div3.className = 'main-img img3';
+    // const div4 = document.createElement('div');
+    // div4.className = 'main-img img4';
+    // const div5 = document.createElement('div');
+    // div5.className = 'main-img img5';
+    
+    // imgBox.appendChild(div5);
+    // imgBox.appendChild(div1);
+    // imgBox.appendChild(div2);
+    // imgBox.appendChild(div3);
+    // imgBox.appendChild(div4);
+    // imgBox.appendChild(div5);
+    // imgBox.appendChild(div1);
+  }
+  makeImg();
 
 // 지유님: 슬라이더 작업섹션
   window.onload = function(){
@@ -78,7 +126,7 @@ tmdb();
         imgBox.style.transition = "all 0.5s linear";
       }
     }
-
+7
     // 자동 슬라이드 
     function startAutoSlide(){
       autoSlide = setInterval(() => {
@@ -100,5 +148,6 @@ tmdb();
     window.rightf = rightf;
     window.tend = tend;
   }
+
 
 // 철원님: 랭킹 작업섹션
