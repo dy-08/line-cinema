92820e2eed5eeba9fe2416a8fd88fb43f0999c51
Youngho Kwon <127571276+dy-08@users.noreply.github.com>
2025-10-22 14:54:18 +0900

    design: 예매 페이지 UI 및 스타일 레이아웃 구현
    브렌치명 변경 
    feat/main-slide -> feat/map
    feat/movie-card -> feat/non-member

--- src/js/pages/upcoming.js
+++ src/js/pages/upcoming.js
@@ -1,8 +1,7 @@
 import { API_KEYS } from '../config/config.js';
 
 export async function fetchUpcomingData() {
-
-  const movies2 = []
+  const movies2 = [];
   const res = await fetch(
     `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEYS.TMDB}&language=ko-KR&page=1`
   );
@@ -34,11 +33,11 @@ export async function fetchUpcomingData() {
   const data5 = await res5.json();
   const datas5 = data5.results;
 
-  movies2.push(...datas)
-  movies2.push(...datas2)
-  movies2.push(...datas3)
-  movies2.push(...datas4)
-  movies2.push(...datas5)
+  movies2.push(...datas);
+  movies2.push(...datas2);
+  movies2.push(...datas3);
+  movies2.push(...datas4);
+  movies2.push(...datas5);
 
   let inBox2 = document.querySelector('.upcoming-inBox2');
 
@@ -77,4 +76,3 @@ export async function fetchUpcomingData() {
     }
   }
 }
-
