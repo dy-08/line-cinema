39dff9ff1849e3a5481276ca91c85b2f0901dda2
Youngho Kwon <127571276+dy-08@users.noreply.github.com>
2025-10-28 05:33:52 +0900

    승아님 비회원 인증 절차 및 팝업 연동 및 비회원 정보 저장 기능 연동
    확인 버튼 클릭 시 게스트 정보 상태 저장 및 폼 초기화 처리
    ---------
    Co-authored-by: seungaaaaa <yg19877@naver.com>

--- src/js/pages/guest.js
+++ src/js/pages/guest.js
@@ -6,4 +6,19 @@ export class Guest {
     this.password = []; // 숫자 4자리
     this.agreedPrivacy = false; // privacy policy 체크여부
   }
+  setName(data) {
+    this.name = data;
+  }
+  setBirth(data) {
+    this.birth = data;
+  }
+  setNumber(data) {
+    this.number = data;
+  }
+  setPassword(data) {
+    this.password = data;
+  }
+  setAgreedPrivacy(data) {
+    this.agreedPrivacy = data;
+  }
 }
